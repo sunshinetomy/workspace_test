c8e2ad14fbc2cfcefdb64cc132d45f7ce2401b3d
DESKTOP-5SFTS5P\rhymus_client_19 <sunshinetomy@gmail.com>
2018-01-22 17:28:09 +0900

    라이브러리 테스트.

---
+++ TestLib/TestLib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib
+{
+    public class Class1
+    {
+		/// <summary>
+		/// Hello1!!!!!!!!!!!!!!!!!!
+		/// </summary>
+		/// <returns></returns>
+		public static string Hello()
+		{
+			return "Hello!! Class1!!";
+		}
+    }
+}
---
+++ TestLib/TestLib/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLib
+{
+	public class Class2
+	{
+		public static string Hello()
+		{
+			return "Hello~? Class2!?!";
+		}
+	}
+}
