ac27210dc9b61d31ee92c01569092194674e3b0f
DESKTOP-5SFTS5P\rhymus_client_19 <sunshinetomy@gmail.com>
2018-01-22 19:05:39 +0900

    # 프로젝트 path 추가.

--- Test_MXL/Test_MXL/Program.cs
+++ Test_MXL/Test_MXL/Program.cs
@@ -5,7 +5,6 @@ using System.Xml;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Test_MXL
 {
@@ -15,9 +14,13 @@ namespace Test_MXL
 		{
 			try
 			{
-				string txtPath = "F:\\workspace_suny\\workspace_test\\Test_MXL\\Test_MXL\\Resources\\";
-				string decompressPath = "F:\\workspace_suny\\workspace_test\\Test_MXL\\Test_MXL\\Resources\\";
-				//string decompressPath = "D:\\workspace_test\\Test_MXL\\Test_MXL\\Resources\\";
+				//Program.TestPath();
+
+				string projectPath = Directory.GetCurrentDirectory() + "\\..\\..\\";
+				string resourcePath = projectPath + "Resources\\";
+				string musicXmlName = "Dichterliebe01.mxl";
+
+				Program.ReadMusicXml( resourcePath + musicXmlName );
 
 				string txtName = "Test_do.txt";
 				
@@ -44,8 +47,7 @@ namespace Test_MXL
 				
 				//DecompressStringFromFile( decompressPath + decompressName );
 
-				string noteData = DecompressToPath( decompressPath + "zipFolder", decompressPath + zipName, decompressPath + "extractFolder" );
-
+				//string noteData = DecompressToPath( decompressPath + "zipFolder", decompressPath + zipName, decompressPath + "extractFolder" );
 
 				//loadXML(decompressPath + decompressName);
 			}
@@ -56,6 +58,17 @@ namespace Test_MXL
 			}
 
 			Console.ReadLine();
+		}
+
+		private static void TestPath()
+		{
+			Console.WriteLine("TestPath: {0}", Directory.GetCurrentDirectory());
+			Console.WriteLine("TestPath: {0}", Directory.GetCurrentDirectory() + "\\..\\..");
+			string[] files = Directory.GetFiles( Directory.GetCurrentDirectory() + "\\..\\.." );
+			for( int index = 0 ; index < files.Length ; ++index )
+			{
+				Console.WriteLine("TestPath: {0}", files[index] );
+			}
 		}
 
 		public static void CompressStringToFile(string fileName, string value)
@@ -248,6 +261,141 @@ namespace Test_MXL
 
 						//XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
 						using (XmlReader reader = XmlReader.Create(new StringReader(noteData)))
+						{
+							XmlWriterSettings ws = new XmlWriterSettings();
+							ws.Indent = true;
+							// Parse the file and display each of the nodes.
+							while (reader.Read())
+							{
+								switch (reader.NodeType)
+								{
+									case XmlNodeType.Element:
+										Console.WriteLine("Element reader.Name: {0}", reader.Name);
+										Console.WriteLine("Element reader.Value: {0}", reader.Value);
+										Console.WriteLine("Element reader.AttributeCount: {0}", reader.AttributeCount);
+										if (true == reader.MoveToFirstAttribute())
+										{
+											Console.WriteLine("Element reader.FirstAttributeName: {0}", reader.Name);
+											Console.WriteLine("Element reader.FirstAttributeValue: {0}", reader.Value);
+											notedataName = reader.Value;
+										}
+										break;
+									case XmlNodeType.Text:
+										Console.WriteLine("Text reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.XmlDeclaration:
+									case XmlNodeType.ProcessingInstruction:
+										Console.WriteLine("XmlDeclaration reader.Name: {0}", reader.Name);
+										Console.WriteLine("XmlDeclaration reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.Comment:
+										Console.WriteLine("Comment reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.EndElement:
+										Console.WriteLine("EndElement");
+										break;
+								}
+							}
+							reader.Close();
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		private static void ReadMusicXmlDocument( string _path )
+		{
+			//XmlDocument xml = new XmlDocument();
+			//xml.LoadXml(containerData);
+			//XmlNodeList xnList = xml.GetElementsByTagName("container"); //접근할 노드
+			//
+			//Console.WriteLine("XmlCount: {0}", xnList.Count);
+			//foreach (XmlNode xn in xnList)
+			//{
+			//	Console.WriteLine("XmlNode: {0}", xn["rootfiles"].InnerXml);
+			//	Console.WriteLine("XmlNode: {0}", xn["rootfiles"]["rootfile"].InnerText);
+			//	//string lng = xn["point"]["y"].InnerText;
+			//}
+		}
+
+		private static string ReadMusicXml( string _path )
+		{
+			using (ZipArchive archive = ZipFile.OpenRead(_path))
+            {
+				string notedataName = null;
+
+				foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+					if( "META-INF/container.xml" == entry.FullName )
+					{
+						Stream fileStream = entry.Open();
+						StreamReader sr = new StreamReader( fileStream, Encoding.UTF8 );
+
+						string containerData = sr.ReadToEnd();
+
+						Console.WriteLine("Name: {0}", entry.FullName);
+						Console.WriteLine("ReadToEnd: {0}", containerData);
+
+						using (XmlReader reader = XmlReader.Create(new StringReader(containerData)))
+						{
+							
+							XmlWriterSettings ws = new XmlWriterSettings();
+							ws.Indent = true;
+							// Parse the file and display each of the nodes.
+							while (reader.Read())
+							{
+								switch (reader.NodeType)
+								{
+									case XmlNodeType.Element:
+										Console.WriteLine("Element reader.Name: {0}", reader.Name);
+										Console.WriteLine("Element reader.Value: {0}", reader.Value);
+										Console.WriteLine("Element reader.AttributeCount: {0}", reader.AttributeCount);
+										if( true == reader.MoveToFirstAttribute() )
+										{
+											Console.WriteLine("Element reader.FirstAttributeName: {0}", reader.Name);
+											Console.WriteLine("Element reader.FirstAttributeValue: {0}", reader.Value);
+											notedataName = reader.Value;
+										}
+										break;
+									case XmlNodeType.Text:
+										Console.WriteLine("Text reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.XmlDeclaration:
+									case XmlNodeType.ProcessingInstruction:
+										Console.WriteLine("XmlDeclaration reader.Name: {0}", reader.Name);
+										Console.WriteLine("XmlDeclaration reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.Comment:
+										Console.WriteLine("Comment reader.Value: {0}", reader.Value);
+										break;
+									case XmlNodeType.EndElement:
+										Console.WriteLine("EndElement");
+										break;
+								}
+							}
+
+							reader.Close();
+						}
+					}
+                }
+
+				foreach (ZipArchiveEntry entry in archive.Entries)
+				{
+					if (notedataName == entry.FullName)
+					{
+						Stream fileStream = entry.Open();
+						StreamReader sr = new StreamReader(fileStream, Encoding.UTF8);
+
+						string noteData = sr.ReadToEnd();
+
+						Console.WriteLine("===================================================");
+						Console.WriteLine("Name: {0}", entry.FullName);
+						//Console.WriteLine("ReadToEnd: {0}", noteData);
+
+						XmlReaderSettings settings = new XmlReaderSettings();
+						settings.DtdProcessing = DtdProcessing.Parse;
+						using (XmlReader reader = XmlReader.Create(new StringReader(noteData), settings))
 						{
 							XmlWriterSettings ws = new XmlWriterSettings();
 							ws.Indent = true;
