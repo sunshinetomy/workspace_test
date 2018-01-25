9952a961b1c8e7296a064607862b39d47297776a
DESKTOP-5SFTS5P\rhymus_client_19 <sunshinetomy@gmail.com>
2018-01-25 18:34:14 +0900

    # TestMxl 유니티 프로젝트 추가.

---
+++ TestMxlUnity/Assets/SceneTestMxl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ionic.Zip;
+
+public class SceneTestMxl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log( "SceneTestMxl - Start" );
+
+		ZipFile zipfile = new ZipFile( Application.streamingAssetsPath + "/89280-Twinkle_Twinkle_Little_Star.mxl", System.Text.Encoding.UTF8 );
+		for( int index = 0 ; index < zipfile.EntryFileNames.Count ; ++index )
+		{
+			Debug.Log( "EntryFileName " + zipfile.EntryFileNames[index] );
+		}
+
+		Debug.Log( "SceneTestMxl - End" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
